d8251a4a00f4586d328b1549f127978ac820b173
Ethanjonm <ethanjonmercado@gmail.com>
2023-04-14 16:38:42 -0400

    added birth_date_cannot_be_future validation to cat model

--- ninety-nine-cats/app/models/cat.rb
+++ ninety-nine-cats/app/models/cat.rb
@@ -12,11 +12,17 @@
 #  updated_at  :datetime         not null
 #
 class Cat < ApplicationRecord
-    CAT_COLORS = [Orange, Tabby, Black, White, Gray, Grey, Brown]
+    CAT_COLORS = ['Orange', 'Tabby', 'Black', 'White', 'Gray', 'Grey', 'Brown']
 
     
     validates :color, :name, :birth_date, :sex, prescence: true
     validates :color, inclusion: {in: CAT_COLORS}
     validates :sex, inclusion: { in: ['M', 'F'] }
+    validate :birth_date_cannot_be_future
 
+    def birth_date_cannot_be_future
+        if birth_date && birth_date > Date.today
+            errors.add(:birth_date, "No time traveling cats.")
+        end
+    end
 end
